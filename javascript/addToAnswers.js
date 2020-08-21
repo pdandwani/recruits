17e5fc7902f7ecb5b85257d6f0ec463fa5500638
Paras Dandwani <43953854+pdandwani@users.noreply.github.com>
2020-08-22 03:35:16 +0530

    Update addToAnswers.js

--- javascript/addToAnswers.js
+++ javascript/addToAnswers.js
@@ -137,10 +137,10 @@ const endSec = startSec;
 //alert(endHour + endMin + endSec);
 var now_now = new Date();
 var date_today_curr = now_now.getDate();
-alert(date_today_curr);
+//alert(date_today_curr);
 // + date_today +
-var countDownDate = new Date("Aug 22 20 " + endHour + ":" + endMin + ":" + endSec).getTime();
-//alert(countDownDate);
+var countDownDate = new Date("Aug "+date_today_curr+" 20 " + endHour + ":" + endMin + ":" + endSec).getTime();
+alert(countDownDate);
 // Update the count down every 1 second
 var x = setInterval(function () {
 
